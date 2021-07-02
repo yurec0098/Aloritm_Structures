dcb6425c17d9390e2c926c2ef35d600198e147f4
yurec0098 <yurec0098@rambler.ru>
2021-07-02 18:42:28 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		static bool IsPrimeNumber(int number)
+		{
+			int d = 0;
+			for (int i = 2; i < number; i++)
+				if (number % i == 0)
+					d++;
+
+			if (d == 0)
+				return true;
+
+			return false;
+		}
+	}
+}
